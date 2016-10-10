e6bc7156c7453c4ab9d8296854a071967cb6554f
JHades <jhades.dev@gmail.com>
2016-10-10 21:02:43 +0200

    router menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import {routerConfig} from "../router.config";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routerConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/router.config.ts
@@ -0,0 +1,22 @@
+
+
+import {Routes} from "@angular/router";
+import {HomeComponent} from "./app/home/home.component";
+
+
+export const routerConfig: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    }
+];
