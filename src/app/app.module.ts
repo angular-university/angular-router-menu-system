957b2250ab06cfb183a391ede40143abba63d17a
JHades <jhades.dev@gmail.com>
2016-10-11 09:10:05 +0200

    router menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import {routerConfig} from "../router.config";
 import {RouterModule} from "@angular/router";
+import { AboutComponent } from './about/about.component';
+import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule,
--- src/router.config.ts
+++ src/router.config.ts
@@ -2,12 +2,22 @@
 
 import {Routes} from "@angular/router";
 import {HomeComponent} from "./app/home/home.component";
+import {AboutComponent} from "./app/about/about.component";
+import {CoursesComponent} from "./app/courses/courses.component";
 
 
 export const routerConfig: Routes = [
     {
         path: 'home',
         component: HomeComponent
+    },
+    {
+        path: 'about',
+        component: AboutComponent
+    },
+    {
+        path: 'courses',
+        component: CoursesComponent
     },
     {
         path: '',
